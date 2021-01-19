20214467ec0779c51c74ab6966086e391be5bdad
Ervin Pepic <ervin.hack@gmail.com>
2021-01-20 00:05:13 +0100

    Django basics

---
+++ myenv/first_project/first_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    my_dict = {
+        'insert_me': "Coming from app templates folder! "
+    }
+    return render(request, 'first_app/index.html', context=my_dict)
